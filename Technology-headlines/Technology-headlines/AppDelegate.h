bcf1081bd60f8656d58a2790777975da5c7c855c
niqang-xiangdangdang <1393167678@qq.com>
2016-06-08 16:38:22 +0800

    准备 -自定义一个控制器继承tabelViewC -SB中新拖建tabelViewC  -关联上一步自定义的类   -嵌入导航控制器  -设置标识符  - 设为默认入口

---
+++ Technology-headlines/Technology-headlines/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Technology-headlines
+//
+//  Created by NIQIANG on 16/6/8.
+//  Copyright © 2016年 NIQIANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
