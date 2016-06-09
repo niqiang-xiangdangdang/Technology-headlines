f49eb12a69f7b99e7c8ebce425b08e47827a3012
niqang-xiangdangdang <1393167678@qq.com>
2016-06-09 21:12:21 +0800

    自定义cell -导入SDWebimage -新建cell -XIB

---
+++ Technology-headlines/Technology-headlines/Cell/ITNewsCell.h
@@ -0,0 +1,13 @@
+//
+//  ITNewsCell.h
+//  Technology-headlines
+//
+//  Created by NIQIANG on 16/6/9.
+//  Copyright © 2016年 NIQIANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITNewsCell : UITableViewCell
+
+@end
---
+++ Technology-headlines/Technology-headlines/Cell/ITNewsCell.m
@@ -0,0 +1,23 @@
+//
+//  ITNewsCell.m
+//  Technology-headlines
+//
+//  Created by NIQIANG on 16/6/9.
+//  Copyright © 2016年 NIQIANG. All rights reserved.
+//
+
+#import "ITNewsCell.h"
+
+@implementation ITNewsCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
