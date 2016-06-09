73e4fb0eb77b9ee70244311cac14374adaf7f21c
niqang-xiangdangdang <1393167678@qq.com>
2016-06-09 23:10:13 +0800

    自定义cell -cell连线 -cell模型设置

--- Technology-headlines/Technology-headlines/Cell/ITNewsCell.m
+++ Technology-headlines/Technology-headlines/Cell/ITNewsCell.m
@@ -7,8 +7,29 @@
 //
 
 #import "ITNewsCell.h"
+#import <UIImageView+WebCache.h>
 
+@interface ITNewsCell()
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *summaryLabel;
+@property (weak, nonatomic) IBOutlet UILabel *siteLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *iconView;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@end
 @implementation ITNewsCell
+
+- (void)setModel:(ITNews *)model{
+    _model = model;
+    
+    _titleLabel.text = model.title;
+    _summaryLabel.text = model.summary;
+    _siteLabel.text = model.sitename;
+    _timeLabel.text = model.docDate;
+    
+    NSURL *url = [NSURL URLWithString:model.src_img];
+    [_iconView sd_setImageWithURL:url];
+    
+}
 
 - (void)awakeFromNib {
     // Initialization code
