3da696597cada3b64c6f609f039a2b1cfdea4c99
niqang-xiangdangdang <1393167678@qq.com>
2016-06-10 01:25:11 +0800

    自定义cell -解决各控件冲突问题 （即完美布局）

--- Technology-headlines/Technology-headlines/Cell/ITNewsCell.m
+++ Technology-headlines/Technology-headlines/Cell/ITNewsCell.m
@@ -15,6 +15,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *siteLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *iconView;
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageWidthCons;
+
+
 @end
 @implementation ITNewsCell
 
@@ -26,9 +30,25 @@
     _siteLabel.text = model.sitename;
     _timeLabel.text = model.docDate;
     
-    NSURL *url = [NSURL URLWithString:model.src_img];
-    [_iconView sd_setImageWithURL:url];
     
+//    NSURL *url = [NSURL URLWithString:model.src_img];
+//    [_iconView sd_setImageWithURL:url];
+    if (model.src_img.length > 0) {
+        NSURL *url = [NSURL URLWithString:model.src_img];
+        [_iconView sd_setImageWithURL:url];
+        
+        _imageWidthCons.constant = 80;
+
+    } else {
+        _imageWidthCons.constant = 0;
+    }
+    
+    
+    [self layoutIfNeeded];
+    
+    BOOL hideSummary = CGRectGetMaxY(_summaryLabel.frame) > CGRectGetMinY(_siteLabel.frame);
+    
+    _summaryLabel.hidden = hideSummary;
 }
 
 - (void)awakeFromNib {
